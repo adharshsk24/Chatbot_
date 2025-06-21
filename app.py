40266f1c510fccac00280822a2950436ceb9a3d6
Adharsh.S <231501006@rajalakshmi.edu.in>
2025-06-21 18:19:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,5 +33,8 @@ def predict():
     response = get_bot_response(message)
     return jsonify({'answer': response})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
